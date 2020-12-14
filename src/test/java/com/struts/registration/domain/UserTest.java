20c045f33d4443d20b4ef80d30002908a742a033
Shamsul Kamal <shamsulkamal.abubakar@gmail.com>
2020-12-15 02:07:10 +0800

    fix unit test

--- src/test/java/com/struts/registration/domain/UserTest.java
+++ src/test/java/com/struts/registration/domain/UserTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -17,6 +19,7 @@ import com.struts.registration.utils.HibernateUtil;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserTest {
     static DaoFactory daoFactory;
+    static Session session;
 
     @BeforeClass
     public static void setUpDaoFactory() {
@@ -50,7 +53,10 @@ public class UserTest {
         user.setEmail("admin@mail.com");
         user.setGender(Gender.MALE);
         user.addHobbyType(HobbyType.SPORT);
+
+        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         daoFactory.getUserDao().save(user);
+        transaction.commit();
 
 //        HibernateUtil.getSessionFactory().openSession().getTransaction().commit();
 
@@ -62,17 +68,23 @@ public class UserTest {
     @Test
     public void test2List() {
         println("test2List");
+        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         List<User> users = daoFactory.getUserDao().findAll();
+        transaction.commit();
         assertEquals(users.size(), 1);
     }
 
     @Test
     public void test3Update() {
         println("test3Update");
+        Transaction transaction = HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+
         User user = daoFactory.getUserDao().findById(1L);
         assertNotNull("User not found", user);
         user.setPassword("password123");
         daoFactory.getUserDao().save(user);
+        
+        transaction.commit();
 
         assertEquals("password123", user.getPassword());
         assertEquals("updater", user.getLastUpdatedBy());
